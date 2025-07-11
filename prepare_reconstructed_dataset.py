4f3f742a32e4e49f90a81c866191014e80a801d2
Khoa <robinkhoa06@gmail.com>
2025-07-10 20:21:01 -0400

    Add multiprocessing support with Pytorch Lightning
    - Replaced custom training logic with Pytorch Lightning to improve
    multiprocessing behavior on large devices
    - Store processed data in HDF5 format for efficiency and scalability

--- prepare_reconstructed_dataset.py
+++ prepare_reconstructed_dataset.py
@@ -1,79 +1,303 @@
 import argparse
-import cv2
-import numpy as np
+import json
 import os
+
+import cv2
+import zarr
+import lightning as L
+import numpy as np
 import torch
+import torch.utils.data as data
 from einops import rearrange
-from tqdm import tqdm
+from lightning.pytorch.callbacks import BasePredictionWriter
 from torch.nn import functional as F
 from torchvision import transforms
-from PIL import Image
 
 from models import VectorQuantizedVAE
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--data-root', type=str, default='data', help='data root for videos')
-    parser.add_argument('-o', '--output', type=str, default='outputs/reconstruction_dataset/', help='output path')
-    parser.add_argument('--ext', type=str, nargs='+', default=['mp4', 'avi', 'mpeg', 'wmv', 'mov', 'flv'], help='target extensions')
-    parser.add_argument('--ckpt', type=str, default='weights/vqvae/model.pth', help='checkpoint path for vqvae')
-    parser.add_argument('--device', type=str, default='cuda', help='device for reconstruction model')
+    parser.add_argument(
+        "-d", "--data-root", type=str, default="data", help="data root for videos"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="outputs/",
+        help="output path",
+    )
+    parser.add_argument(
+        "--ext",
+        type=str,
+        nargs="+",
+        default=["mp4", "avi", "mpeg", "wmv", "mov", "flv"],
+        help="target extensions",
+    )
+    parser.add_argument(
+        "--ckpt",
+        type=str,
+        default="weights/vqvae/model.pt",
+        help="checkpoint path for vqvae",
+    )
+    parser.add_argument(
+        "--device", type=str, default="cuda", help="device for reconstruction model"
+    )
     return parser.parse_args()
 
 
-def denormalize_batch_t(img_t, mean, std):
-    try:
-        assert(len(mean) == len(std))
-        assert(len(mean) == img_t.shape[1])
-    except:
-        print(f'Unmatched channels between image tensors and normalization mean and std. Got {img_t.shape[1]}, {len(mean)}, {len(std)}.')
-    img_denorm = torch.empty_like(img_t)
-    for t in range(img_t.shape[1]):
-        img_denorm[:, t, :, :] = (img_t[:, t, :, :].clone() * std[t]) + mean[t]
-    return img_denorm
-   
-   
-if __name__ == '__main__':
-    args = parse_args()
-    cls_folders = list(filter(lambda p: os.path.isdir(os.path.join(args.data_root, p)), sorted(os.listdir(args.data_root))))
-    recons_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    ])
-    print(f'Find {len(cls_folders)} classes: {cls_folders}')
-    model = VectorQuantizedVAE(3, 256, 512)
-    state_dict = torch.load('weights/vqvae/model.pt', map_location=args.device)
-    model.load_state_dict(state_dict, strict=True)
-    model.to(args.device)
-    for idx, cls_folder in enumerate(cls_folders, 1):
-        os.makedirs(os.path.join(args.output, cls_folder, 'original'), exist_ok=True)
-        video_list = list(filter(lambda fn: os.path.splitext(fn)[-1].lower()[1:] in args.ext, sorted(os.listdir(os.path.join(args.data_root, cls_folder)))))
-        for video in tqdm(video_list, desc='Extracting original frames'):
-            video_path = os.path.join(args.data_root, cls_folder, video)
+class AV1MDataModule(L.LightningDataModule):
+
+    def __init__(self, metadata_file, data_root, batch_size, num_workers):
+        super().__init__()
+        self.metadata_file = metadata_file
+        self.data_root = data_root
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def setup(self, stage):
+        with open(self.metadata_file, "r") as file:
+            self.metadata = json.load(file)
+        self.metadata = [
+            os.path.join(self.data_root, video_info["file"])
+            for video_info in self.metadata
+            if os.path.exists(os.path.join(self.data_root, video_info["file"]))
+        ]
+
+    def predict_dataloader(self):
+        return data.DataLoader(
+            self.metadata, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+
+class HDF5Dataset(data.Dataset):
+    def __init__(self, file_path, output_hdf5):
+        self.file_path = file_path
+        self.output_hdf5 = output_hdf5
+
+        self.index_to_dataset = []
+        with h5py.File(os.path.join(self.file_path, self.output_hdf5), "w") as file:
+            for hdf5_file in os.listdir(file_path):
+                if "hdf5" in hdf5_file and hdf5_file != output_hdf5:
+                    file[hdf5_file] = h5py.ExternalLink(
+                        os.path.join(self.file_path, hdf5_file), "/"
+                    )
+                    for video in list(file[hdf5_file].keys()):
+                        self.index_to_dataset.append(os.path.join(hdf5_file, video))
+
+        self.dataset = None
+
+    def __getitem__(self, index):
+        if self.dataset is None:
+            self.dataset = h5py.File(
+                os.path.join(self.file_path, self.output_hdf5), "r"
+            )
+        return (
+            self.index_to_dataset[index],
+            np.array(self.dataset[self.index_to_dataset[index]]),
+        )
+
+    def __len__(self):
+        return len(self.index_to_dataset)
+
+    def close(self):
+        if self.dataset is not None:
+            self.dataset.close()
+            self.dataset = None
+
+
+class ReconstructDataModule(L.LightningDataModule):
+
+    def __init__(self, file_path, output_hdf5, batch_size, num_workers):
+        super().__init__()
+        self.file_path = file_path
+        self.output_hdf5 = output_hdf5
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def prepare_data(self):
+        self.dataset = HDF5Dataset(self.file_path, self.output_hdf5)
+
+    def predict_dataloader(self):
+        return data.DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=lambda batch: batch,
+        )
+
+    def teardown(self, stage):
+        self.dataset.close()
+
+
+class CustomWriter(BasePredictionWriter):
+    def __init__(self, output_path, hdf5_filename):
+        super().__init__("batch")
+        self.output_path = output_path
+        self.hdf5_filename = hdf5_filename
+
+    def write_on_batch_end(
+        self,
+        trainer,
+        pl_module,
+        prediction,
+        batch_indices,
+        batch,
+        batch_idx,
+        dataloader_idx,
+    ):
+        if prediction == None:
+            print(batch)
+            return
+        with zarr.open(
+            os.path.join(
+                self.output_path, str(trainer.global_rank) + "_" + self.hdf5_filename
+            ),
+            "a",
+        ) as file:
+            for key, value in prediction.items():
+                if key in file:
+                    file[key][...] = value
+                else:
+                    file.create_dataset(
+                        key,
+                        data=value,
+                        # chunks=True,
+                        compression="gzip",
+                        # compression_opts=6,
+                    )
+
+
+class VideoFrameExtractor(L.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    def predict_step(self, batch):
+        extracted_batch = {}
+        for video_path in batch:
             vc = cv2.VideoCapture(video_path)
-            if vc.isOpened():
-                rval, frame = vc.read()
-            else:
-                rval = False
-            c = 1
-            while rval:
-                cv2.imwrite(os.path.join(args.output, cls_folder, 'original', f'{os.path.splitext(os.path.basename(video))[0]}_{c}.jpg'), frame)
-                c = c + 1
-                rval, frame = vc.read()
+
+            if not vc.isOpened():
+                continue
+
+            extracted_frames = []
+            while True:
+                ret, frame = vc.read()
+                if not ret:
+                    break
+                extracted_frames.append(frame)
             vc.release()
-        os.makedirs(os.path.join(args.output, cls_folder, 'recons'), exist_ok=True)
-        for image_fn in tqdm(sorted(os.listdir(os.path.join(args.output, cls_folder, 'original'))), desc='Reconstruction'):
-            img = Image.open(os.path.join(args.output, cls_folder, 'original', image_fn)).convert('RGB')
-            img = recons_transform(img).unsqueeze(0)
-            with torch.no_grad():
-                img = img.to(args.device)
-                recons, _, _ = model(img)
-                if recons.shape != img.shape:
-                    recons = F.interpolate(recons, (img.shape[-2], img.shape[-1]), mode='nearest')
-                denorm_recons = denormalize_batch_t(recons, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-                denorm_recons = rearrange(denorm_recons.squeeze(0).cpu().numpy(), 'c h w -> h w c')
-                recons_img = np.uint8(denorm_recons * 255.)
-                Image.fromarray(recons_img).save(os.path.join(args.output, cls_folder, 'recons', image_fn))
-        print(f'Finished {idx}/{len(cls_folders)}')
-    print(f'Finished. Results saved at {args.output}')
+
+            extracted_batch[video_path.replace("/", "_")] = np.array(extracted_frames)
+        return extracted_batch
+
+
+class VectorQuantizedVAEWrapper(L.LightningModule):
+
+    def __init__(self, ckpt, batch_size):
+        super().__init__()
+        self.recons_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+            ]
+        )
+
+        self.model = VectorQuantizedVAE(3, 256, 512)
+        self.model.load_state_dict(torch.load(ckpt), strict=True)
+
+        self.batch_size = batch_size
+
+    def denormalize_batch_t(self, img_t, mean, std):
+        try:
+            assert len(mean) == len(std)
+            assert len(mean) == img_t.shape[1]
+        except:
+            print(
+                f"Unmatched channels between image tensors and normalization mean and std. Got {img_t.shape[1]}, {len(mean)}, {len(std)}."
+            )
+        img_denorm = torch.empty_like(img_t)
+        for t in range(img_t.shape[1]):
+            img_denorm[:, t, :, :] = (img_t[:, t, :, :].clone() * std[t]) + mean[t]
+        return img_denorm
+
+    def forward(self, X):
+        return self.model(X)
+
+    def predict_step(self, batch):
+        reconstructed_batch = {}
+        for video_path, extracted_frames in batch:
+            reconstructed_frames = np.zeros(extracted_frames.shape)
+            for i in range(0, extracted_frames.shape[0], self.batch_size):
+                frames_batch = torch.stack(
+                    [
+                        self.recons_transform(frame)
+                        for frame in extracted_frames[i : i + self.batch_size]
+                    ]
+                )
+                frames_batch = frames_batch.to(next(self.model.parameters()).device)
+                reconstructed_frames_batch, _, _ = model(frames_batch)
+
+                if reconstructed_frames_batch.shape != frames_batch.shape:
+                    reconstructed_frames_batch = F.interpolate(
+                        reconstructed_frames_batch,
+                        (frames_batch.shape[-2], frames_batch.shape[-1]),
+                        mode="nearest",
+                    )
+                reconstructed_frames_batch = self.denormalize_batch_t(
+                    reconstructed_frames_batch,
+                    mean=[0.5, 0.5, 0.5],
+                    std=[0.5, 0.5, 0.5],
+                )
+                reconstructed_frames_batch = rearrange(
+                    reconstructed_frames_batch.cpu().numpy(),
+                    "b c h w -> b h w c",
+                )
+                reconstructed_frames_batch = np.uint8(
+                    reconstructed_frames_batch * 255.0
+                )
+                reconstructed_frames[i : i + self.batch_size] = (
+                    reconstructed_frames_batch
+                )
+            reconstructed_batch[video_path] = reconstructed_frames
+        return reconstructed_batch
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    original_path = os.path.join(args.output, "original")
+    reconstruct_path = os.path.join(args.output, "reconstruct")
+
+    os.makedirs(original_path, exist_ok=True)
+    os.makedirs(reconstruct_path, exist_ok=True)
+
+    if True:
+        av1m_datamodule = AV1MDataModule(
+            metadata_file="data/train_metadata.json",
+            data_root="data/train/train",
+            batch_size=8,
+            num_workers=16,
+        )
+        video_frame_extractor = VideoFrameExtractor()
+        trainer = L.Trainer(
+            accelerator="cpu",
+            devices=4,
+            callbacks=[CustomWriter(original_path, "original.hdf5")],
+        )
+        trainer.predict(
+            video_frame_extractor, av1m_datamodule, return_predictions=False
+        )
+    if True:
+        reconstruct_datamodule = ReconstructDataModule(
+            file_path=original_path,
+            output_hdf5="original.hdf5",
+            batch_size=6,
+            num_workers=16,
+        )
+        model = VectorQuantizedVAEWrapper(ckpt=args.ckpt, batch_size=64)
+        trainer = L.Trainer(
+            callbacks=[CustomWriter(reconstruct_path, "reconstruct.hdf5")]
+        )
+        trainer.predict(model, reconstruct_datamodule, return_predictions=False)
